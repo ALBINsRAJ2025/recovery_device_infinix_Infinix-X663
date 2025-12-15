833545c0b43eb6185fa3813bbdf70f0a02082f28
ALBINsRAJ2025 <albinsraj2025@gmail.com>
2025-12-15 14:39:19 +0530

    X663 : PBRP Bringup

--- pb_X663.mk
+++ pb_X663.mk
@@ -3,15 +3,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from Infinix-X663 device
 $(call inherit-product, device/infinix/Infinix-X663/device.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Product Specifics
-PRODUCT_NAME := twrp_X663
+PRODUCT_NAME := pb_X663
 PRODUCT_DEVICE := Infinix-X663
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X663
